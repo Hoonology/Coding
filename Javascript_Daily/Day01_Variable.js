1216781feefffae31a83219e96639e20b7abfe78
Hoony <mase306@gmail.com>
2023-03-17 17:22:29 +0900

    Day01_VariableBasic

---
+++ Javascript_Daily/Day01_Variable.js
@@ -0,0 +1,15 @@
+// 문제 1-2. TODO : 키워드 let을 사용하여 변수 course를 선언하세요.
+let course1;
+
+// 문제 1-2. 선언되어 있는 변수 course에 문자열 'code states DevOps'를 할당하세요.
+let course; // TODO : 선언되어 있는 변수 course에 문자열 'code states DevOps'를 할당하세요.
+course = 'code states DevOps';
+
+// 문제 1-3. 변수 num1에 숫자 5를 할당하고, 변수 num2에 숫자 7을 할당한 후, 
+// 변수 result에 숫자 num1과 숫자 num2의 곱을 할당합니다.
+
+let num1, num2, result;
+// TODO : 변수 num1에 숫자 5를 할당하고, 변수 num2에 숫자 7을 할당한 후, 변수 result에 숫자가 할당된 변수 num1과 num2의 곱을 할당합니다.
+num1 = 5;
+num2 = 7;
+result = num1 * num2;
