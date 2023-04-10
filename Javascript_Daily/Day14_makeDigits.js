590d9fd2764ec236450eb39879e15839abc41e68
Hoony <mase306@gmail.com>
2023-04-10 10:10:09 +0900

    day14

--- Javascript_Daily/Day14_makeDigits.js
+++ Javascript_Daily/Day14_makeDigits.js
@@ -1,6 +1,13 @@
 /* makeDigits
 문제
-ㅊ
+수(num)를 입력받아 1부터 num까지의 정수로 구성된 문자열을 리턴해야 합니다.
+
+출력
+string 타입을 리턴해야 합니다.
+
+주의 사항
+반복문(for)문을 사용해야 합니다.
+숫자(number string) 사이의 구분은 없습니다. ('1234567')
 
 let output = makeDigits(5);
 console.log(output); // --> "12345"
